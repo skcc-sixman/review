7c4cb7201e13a8dd7dc89fc2ceede1f8540ff888
RBJH <jhyun9356@gmail.com>
2022-09-11 16:26:46 +0900

    review

--- src/main/java/com/recofit/review/controller/ReviewController.java
+++ src/main/java/com/recofit/review/controller/ReviewController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,9 +44,18 @@ public class ReviewController {
   }
 
   @GetMapping(value = "/reviews")
-  public ResponseEntity<Response> getReviews(@RequestParam(required = false, value = "target-type") TargetType targetType) {
-    Long userId = 1L;
-    UserType userType = UserType.USER;
+  public ResponseEntity<Response> getReviews(@RequestHeader(value = "User-Id") Long uId, @RequestHeader(value = "User-Type") String uType, @RequestParam(required = false, value = "target-type") TargetType targetType) {
+    Long userId = (Long) uId;
+    UserType userType  = UserType.USER;
+
+    if("TRAINER".equals(uType)) {
+      userType = UserType.TRAINER;
+    }
+
+    if("GYMOWNER".equals(uType)) {
+      userType = UserType.GYM;
+    }
+
     List<ReviewDTO> reviewDTOs = Collections.emptyList();
 
     Long targetId = userId;
